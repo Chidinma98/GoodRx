7867f4a26e8ff3f75ff8350263f3f89cd9bca38a
Open Account <openaccount@Admins-MacBook-Air.local>
2019-07-09 21:42:01 -0400

    routes setup

--- goodrx-app/config/routes.rb
+++ goodrx-app/config/routes.rb
@@ -1,10 +1,18 @@
 Rails.application.routes.draw do
 
-  resources :prescriptions
+  # resources :prescriptions
   resources :doctors
   post '/auth/login', to: 'authentication#login'
 
 
-  resources :users
+  resources :users do
+    resources :prescriptions
+
+  end
+  
+  
+  
+  
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
