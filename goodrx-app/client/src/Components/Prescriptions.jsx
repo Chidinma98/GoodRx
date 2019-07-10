2223c8c7fcb89ae78e436c362841af2c9b98fcbf
Open Account <openaccount@Admins-MacBook-Air.local>
2019-07-10 14:55:46 -0400

    maps over prescriptions

--- goodrx-app/client/src/Components/Prescriptions.jsx
+++ goodrx-app/client/src/Components/Prescriptions.jsx
@@ -5,19 +5,32 @@ import axios from 'axios'
 class Prescriptions extends Component{
 constructor(props){
 super(props)
+this.state = {
+    prescriptions: []
+}
+
 }
 
 async componentDidMount(){
 
-    const prescriptions = await axios.get(`http://localhost:3000/users/${this.props.id}/prescriptions`)
-    console.log(prescriptions)
+    const medications = await axios.get(`http://localhost:3000/users/${this.props.id}/prescriptions`)
+this.setState({
+    prescriptions: medications.data
+})
 
 }
 
 
     render(){
+         const prescriptions = this.state.prescriptions
+        
         return(<div>
+           
+<h1>{prescriptions.map((prescription, index)=>{
+return(<p key = {prescription.id}>{prescription.name}</p>)
 
+
+})}</h1>
            
         </div>)
     }
