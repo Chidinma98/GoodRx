097ba460aa1d226fa8278763bf4134d06f2ed5c9
Open Account <openaccount@Admins-MacBook-Air.local>
2019-07-13 10:28:18 -0400

    material signup login

--- client/src/Components/SignUpForm.jsx
+++ client/src/Components/SignUpForm.jsx
@@ -1,6 +1,43 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+// import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
 
+const useStyles = makeStyles(theme => ({
+    '@global': {
+      body: {
+        backgroundColor: theme.palette.common.white,
+      },
+    },
+    paper: {
+      marginTop: theme.spacing(8),
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+    },
+    avatar: {
+      margin: theme.spacing(1),
+      backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+      width: '100%', // Fix IE 11 issue.
+      marginTop: theme.spacing(3),
+    },
+    submit: {
+      margin: theme.spacing(3, 0, 2),
+    },
+  }));
 
 class SignUpForm extends Component {
     state = {
@@ -34,53 +71,117 @@ class SignUpForm extends Component {
     }
 
     render() {
+        const { classes } = this.props
         return (
-            <div>
-                <h2>Sign Up</h2>
-                <form onSubmit={this.handleSubmit}>
-                    <p>Username</p>
-                    <input
-                        name="username"
-                        type="text"
-                        value={this.state.username}
-                        onChange={this.handleChange}
+            <Container component="main" maxWidth="xs">
+            <CssBaseline />
+            <div className={this.props.paper}>
+              <Avatar className={this.props.avatar}>
+              </Avatar>
+              <Typography component="h1" variant="h5">
+                Sign up
+              </Typography>
+              <form onSubmit={this.handleSubmit} className={this.props.form} noValidate>
+                <Grid container spacing={2}>
+                  <Grid item xs={12} >
+                    <TextField
+                      value={this.state.username}
+                      onChange={this.handleChange}
+                      autoComplete="fname"
+                      name="username"
+                      variant="outlined"
+                      required
+                      fullWidth
+                      id="username"
+                      label="Username"
+                      autoFocus
                     />
-
-                    <p>Email:</p>
-                    <input
-                        name="email"
-                        type="email"
-                        value={this.state.email}
-                        onChange={this.handleChange}
+                  </Grid>
+                  <Grid item xs={12} >
+                    <TextField
+                    value={this.state.email}
+                    onChange={this.handleChange}
+                      variant="outlined"
+                      required
+                      fullWidth
+                      id="email"
+                      label="email"
+                      name="email"
+                      autoComplete="email"
                     />
-
-                    <p>Phone Number:</p>
-                    <input
-                        name="phone_number"
-                        type="integer"
-                        value={this.state.phone_number}
-                        onChange={this.handleChange}
+                  </Grid>
+                  <Grid item xs={12}>
+                    <TextField
+                    value={this.state.phone_number}
+                    onChange={this.handleChange}
+                    type="integer"
+                      variant="outlined"
+                      required
+                      fullWidth
+                      id="phone_number"
+                      label="phone_number"
+                      name="phone_number"
+                      autoComplete="phone_number"
+                      
                     />
-                    <p>Password:</p>
-                    <input
-                        name="password"
-                        type="password"
-                        value={this.state.password}
-                        onChange={this.handleChange}
+                  </Grid>
+                  <Grid item xs={12}>
+                    <TextField
+                    value={this.state.password}
+                    onChange={this.handleChange}
+                      variant="outlined"
+                      required
+                      fullWidth
+                      name="password"
+                      label="Password"
+                      type="password"
+                      id="password"
+                      autoComplete="current-password"
                     />
-                    <p>Confirm Password</p>
-                    <input
-                        name="passwordConfirm"
-                        type="password"
-                        value={this.state.passwordConfirm}
-                        onChange={this.handleChange}
+                  </Grid>
+                  <Grid item xs={12}>
+                     <TextField
+                    value={this.state.passwordConfirm}
+                    onChange={this.handleChange}
+                      variant="outlined"
+                      required
+                      fullWidth
+                      name="passwordConfirm"
+                      type="password"
+                      label="Confirm Password"
+                      id="password"
+                      autoComplete="current-password"
                     />
-                    <br />
-                    <button>Sign Up</button>
-
-                </form>
-
+                  
+                  
+                  </Grid>
+                  <Grid item xs={12}>
+                    <FormControlLabel
+                      control={<Checkbox value="allowExtraEmails" color="primary" />}
+                      label="I want to receive inspiration, marketing promotions and updates via email."
+                    />
+                  </Grid>
+                </Grid>
+                <Button
+                  type="submit"
+                  fullWidth
+                  variant="contained"
+                  color="primary"
+                  className={this.props.submit}
+                >
+                  Sign Up
+                </Button>
+                <Grid container justify="flex-end">
+                  <Grid item>
+                    <Link href="#" variant="body2">
+                      Already have an account? Sign in
+                    </Link>
+                  </Grid>
+                </Grid>
+              </form>
             </div>
+           
+          </Container>
 
         )
 
