fb2a202a43c38cec7ff47c2decf5f84d96aa01cc
Open Account <openaccount@Admins-MacBook-Air.local>
2019-07-14 08:11:28 -0400

    show route working for meds

--- client/src/Components/Medication.jsx
+++ client/src/Components/Medication.jsx
@@ -6,6 +6,15 @@ import axios from 'axios'
 class Medication extends Component{
 constructor(){
 super()
+this.state = {
+    time:"",
+    name:"",
+    pharmacy:"",
+    prescriber:"",
+    prescriber_phone:"",
+    specialty:"",
+    purpose:""
+}
 
 
 
@@ -14,14 +23,26 @@ super()
 
 async componentDidMount(){
 const res = await axios.get(`http://localhost:3000/${this.props.history.location.pathname}`)
-console.log(res)
+const prescription = res.data.med
+// console.log(prescription)
+this.setState({
+    time:prescription.medication_time,
+    name: prescription.name,
+    pharmacy: prescription.pharmacy,
+    prescriber: prescription.prescriber,
+    prescriber_phone: prescription.prescriber_phone,
+    specialty:prescription.prescriber_specialization,
+    purpose:prescription.purpose
+})
 
 
 }
 
 
     render(){
-         
+        const med = this.state
+        console.log(med.name)
+        
         
         return(<div>
 
