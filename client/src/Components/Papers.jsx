a90f518fd9d52d59a617c06fbeb95a34342194ec
Open Account <openaccount@Admins-MacBook-Air.local>
2019-07-14 11:13:10 -0400

    show route ok

--- client/src/Components/Papers.jsx
+++ client/src/Components/Papers.jsx
@@ -1,5 +1,5 @@
-import React, {Component} from "react";
-import {Route, Link, Redirect} from "react-router-dom";
+import React, { Component } from "react";
+import { Route, Link, Redirect } from "react-router-dom";
 import axios from "axios";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
@@ -7,7 +7,7 @@ import Paper from "@material-ui/core/Paper";
 import Grid from '@material-ui/core/Grid';
 
 
-import {withStyles} from "@material-ui/core/styles";
+import { withStyles } from "@material-ui/core/styles";
 import "./Paper.css";
 
 
@@ -24,49 +24,50 @@ class Papers extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      time:"",
-      dosage:"",
-      name:"",
-      pharmacy:"",
-      prescriber:"",
-      prescriber_phone:"",
-      specialty:"",
-      purpose:""
-  }
-  
-  
+      time: "",
+      dosage: "",
+      name: "",
+      pharmacy: "",
+      prescriber: "",
+      prescriber_phone: "",
+      specialty: "",
+      purpose: ""
+    }
+
+
   }
 
-  
-  async componentDidMount(){
+
+  async componentDidMount() {
     const res = await axios.get(`http://localhost:3000/${this.props.history.location.pathname}`)
     const prescription = res.data.med
     // console.log(prescription)
     this.setState({
-        time:prescription.medication_time,
-        name: prescription.name,
-        pharmacy: prescription.pharmacy,
-        prescriber: prescription.prescriber,
-        prescriber_phone: prescription.prescriber_phone,
-        specialty:prescription.prescriber_specialization,
-        purpose:prescription.purpose,
-        dosage: prescription.dosage
+      time: prescription.medication_time,
+      name: prescription.name,
+      pharmacy: prescription.pharmacy,
+      prescriber: prescription.prescriber,
+      prescriber_phone: prescription.prescriber_phone,
+      specialty: prescription.prescriber_specialization,
+      purpose: prescription.purpose,
+      dosage: prescription.dosage
     })
-    
-    
-    }
-  
-  
-  
-  
-  
+
+
+  }
+
+
+
+
+
   render() {
     const med = this.state
-        console.log(med.name)
+    
     return (
       <>
-       
+       <Button onClick={() => { this.props.history.goBack() }} variant="contained" color="primary">Back</Button>
         <div className="add-update-product">
+      
           <div className="paper-container">
             <StyledPaper
               className="paper-add-product"
@@ -77,85 +78,85 @@ class Papers extends Component {
                 <h2>{med.name}</h2>
 
                 <form className="add-update-form">
-                <Grid container spacing={3}>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  name="dosage"
-                  label="Dosage"
-                  fullWidth
-                  autoComplete="fname"
-                  value={med.dosage}
-                />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <TextField
-               
-                  
-                  name="medication_time"
-                  type = "text"
-                  label="Time Taken"
-                  value= {med.time}
-                  fullWidth
-                  autoComplete="lname"
-                />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  name="purpose"
-                  label="Purpose"
-                  value = {med.purpose}
-                  fullWidth
-                 
-                />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                
-                  name="pharmacy"
-                  label="Pharmacy"
-                  value = {med.pharmacy}
-                  fullWidth
-                  
-                />
-              </Grid>
-              <Grid item xs={12} >
-                <TextField
-                 
-                  
-                  name="prescriber"
-                  label= "Prescriber"
-                  value = {`Dr. ${med.prescriber}`}
-                  fullWidth
-                
-                />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <TextField 
-                id="state" 
-                name="prescriber_specialization" 
-                label="Specialization" 
-      
-                value = {med.specialty}
-                fullWidth />
-              </Grid>
-           
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  
-                 
-                  name="prescriber_specialization"
-                  label="Phone Number"
-                  value = {med.prescriber_phone}
-                  fullWidth
-                  
-                />
-              </Grid>
+                  <Grid container spacing={3}>
+                    <Grid item xs={12} sm={6}>
+                      <TextField
+                        name="dosage"
+                        label="Dosage"
+                        fullWidth
+                        autoComplete="fname"
+                        value={med.dosage}
+                      />
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                      <TextField
 
-              
-             
-            </Grid>
 
-                  
+                        name="medication_time"
+                        type="text"
+                        label="Time Taken"
+                        value={med.time}
+                        fullWidth
+                        autoComplete="lname"
+                      />
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                      <TextField
+                        name="purpose"
+                        label="Purpose"
+                        value={med.purpose}
+                        fullWidth
+
+                      />
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                      <TextField
+
+                        name="pharmacy"
+                        label="Pharmacy"
+                        value={med.pharmacy}
+                        fullWidth
+
+                      />
+                    </Grid>
+                    <Grid item xs={12} >
+                      <TextField
+
+
+                        name="prescriber"
+                        label="Prescriber"
+                        value={`Dr. ${med.prescriber}`}
+                        fullWidth
+
+                      />
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                      <TextField
+                        id="state"
+                        name="prescriber_specialization"
+                        label="Specialization"
+
+                        value={med.specialty}
+                        fullWidth />
+                    </Grid>
+
+                    <Grid item xs={12} sm={6}>
+                      <TextField
+
+
+                        name="prescriber_specialization"
+                        label="Phone Number"
+                        value={med.prescriber_phone}
+                        fullWidth
+
+                      />
+                    </Grid>
+
+
+
+                  </Grid>
+
+
                 </form>
               </div>
 
