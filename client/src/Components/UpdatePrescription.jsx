77d8a1ee9150c4032a4c84ea1d611eaa57a10aac
Open Account <openaccount@Admins-MacBook-Air.local>
2019-07-14 15:58:12 -0400

    edit form created

--- client/src/Components/UpdatePrescription.jsx
+++ client/src/Components/UpdatePrescription.jsx
@@ -1,43 +1,68 @@
 import React, { Component } from "react";
-import { Route, Link, Redirect } from "react-router-dom";
-import axios from "axios";
+import { Link } from "react-router-dom";
+import axios from 'axios'
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
 import Paper from "@material-ui/core/Paper";
 import Grid from '@material-ui/core/Grid';
+import { withStyles } from "@material-ui/core/styles";
+import "./Paper.css";
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
 
-
-import { withStyles } from "@material-ui/core/styles";
-import "./Paper.css";
-
-
-
 const StyledPaper = withStyles({
-  root: {
-  }
-
-})(Paper);
-
-
-
-class UpdatePrescription extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      time: "",
-      dosage: "",
-      name: "",
-      pharmacy: "",
-      prescriber: "",
-      prescriber_phone: "",
-      specialty: "",
-      purpose: ""
+    root: {
     }
+  
+  })(Paper);
 
 
-  }
+class UpdatePrescription extends Component{
+constructor(){
+super()
+
+this.state = {
+name:"",
+dosage:"",
+medication_time:"",
+purpose:"",
+pharmacy:"",
+prescriber:"",
+prescriber_specialization:"",
+prescriber_phone:""
+
+}
+
+this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+
+}
+
+handleChange(event){
+    const name = event.target.name;
+    const value = event.target.value
+    this.setState({
+        [name]: value
+    })
+
+}
+
+async handleSubmit(e){
+    e.preventDefault()
+    await axios.put(`http://localhost:3000/users/${this.props.id}/prescriptions`,{
+name: this.state.name,
+dosage:this.state.dosage,
+medication_time:this.state.medication_time,
+purpose: this.state.purpose,
+pharmacy: this.state.pharmacy,
+prescriber: this.state.prescriber,
+prescriber_specialization:this.state.prescriber_specialization,
+prescriber_phone:this.state.prescriber_phone
+
+    })
+
+}
+
 
 
   async componentDidMount() {
@@ -66,22 +91,32 @@ class UpdatePrescription extends Component {
     const med = this.state
 
     return (
-      <>
+        <>
         <Button onClick={() => { this.props.history.goBack() }} variant="contained" color="primary">Back</Button>
-        <div className="add-update-product">
-
-          <div className="paper-container">
-            <StyledPaper
-              className="paper-add-product"
-              component="div"
-              elevation="3"
-            >
-              <div className="add-update-container">
-                <h2>{med.name}</h2>
-
-                <form className="add-update-form">
-                  <Grid container spacing={3}>
-                    <Grid item xs={12} sm={6}>
+         <div className="add-update-product">
+       
+           <div className="paper-container">
+             <StyledPaper
+               className="paper-add-product"
+               component="div"
+               elevation="3"
+             >
+               <div className="add-update-container">
+                 <h2>Edit Prescription</h2>
+ 
+                 <form className="add-update-form" onChange = {this.handleChange} onSubmit = {this.handleSubmit}>
+                   <Grid container spacing={3}>
+                     <Grid item xs={12} >
+                       <TextField
+                         name="name"
+                         label="Prescription Name"
+                         fullWidth
+                         autoComplete="fname"
+                         value = {med.name}
+                         
+                       />
+                     </Grid>
+                     <Grid item xs={12} sm={6}>
                       <TextField
                         name="dosage"
                         label="Dosage"
@@ -153,34 +188,25 @@ class UpdatePrescription extends Component {
 
                       />
                     </Grid>
+ 
+ 
+                   </Grid>
+                   <br/>
+                   <FormControlLabel
+        control={<Checkbox value="remember" color="primary" />}
+        label="Set Alert"
+      />
 
-
-
-                  </Grid>
-
-
-                </form>
-              </div>
-              <br/>
-
-              <FormControlLabel
-            control={<Checkbox value="remember" color="primary" />}
-            label="Set Alert"
-          />
-
-
-              <br />
-              <nav> <Button>Edit</Button> <Button>Delete</Button> </nav>
-
-
-
-              <br />
-
-
-            </StyledPaper>
-          </div>
-        </div>
-      </>
+                   <br/>
+ 
+ <Button type = "submit" color="primary">Update</Button>
+                 </form>
+               </div>
+ 
+             </StyledPaper>
+           </div>
+         </div>
+       </>
     );
   }
 }
